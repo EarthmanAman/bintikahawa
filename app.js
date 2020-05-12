8c4679dc0d52017fccffb32b090a0c45a2a0b289
EarthmanAman <hashimathman@gmail.com>
2020-05-12 21:43:18 +0300

    New website

---
+++ app.js
@@ -0,0 +1,21 @@
+const navSlideC2 = () => {
+	const burger = document.querySelector(".burgerC2");
+	const nav = document.querySelector(".nav-linksC2");
+	const navLinks = document.querySelectorAll(".nav-linksC2 li");
+	burger.addEventListener("click", ()=>{
+		nav.classList.toggle("nav-activeC2");
+
+		navLinks.forEach((link, index)=>{
+			if(link.style.animation){
+				link.style.animation = "";
+			}else{
+				link.style.animation = `navLinkFadeC2 0.5s ease forwards ${index / 7 + 0.8}s`
+			}
+		})
+		
+	})
+
+
+}
+
+navSlideC2();
